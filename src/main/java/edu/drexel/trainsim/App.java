cd09b0884f0de6987a6cfc0249b85a4ca9def1dc
Jason Lefever <jtl86@drexel.edu>
2021-07-03 14:44:21 -0400

    Create maven project

---
+++ src/main/java/edu/drexel/trainsim/App.java
@@ -0,0 +1,13 @@
+package edu.drexel.trainsim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

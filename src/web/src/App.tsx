ae70bd78335729227ca3137b064aca9afcc0c19f
Jason Lefever <jtl86@drexel.edu>
2021-07-05 19:48:23 -0400

    Add bulma and fontawesome

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
 export default () => {
-    return <span>Hello, World!</span>;
+    return <div className="has-text-centered">
+        <h1 className="title">Title</h1>
+        <p className="subtitle">Subtitle</p>
+        <i className="fas fa-dog"></i>
+    </div>;
 };
--- src/web/webpack.config.js
+++ src/web/webpack.config.js
@@ -10,8 +10,20 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
+                use: "ts-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            },
+            {
+
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
             }
         ]
     },

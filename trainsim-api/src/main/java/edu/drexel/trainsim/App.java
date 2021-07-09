dcf0d9ce629ca64fd959769c85f0e7b699d23c74
Jason Lefever <jtl86@drexel.edu>
2021-07-09 15:50:28 -0400

    Load database connection info from environment variables

--- trainsim-api/src/main/java/edu/drexel/trainsim/App.java
+++ trainsim-api/src/main/java/edu/drexel/trainsim/App.java
@@ -6,12 +6,9 @@ import edu.drexel.trainsim.models.Query;
 import edu.drexel.trainsim.queries.GetAllStops;
 import io.javalin.Javalin;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        Sql2o sql2o = new Sql2o("jdbc:postgresql://trainsim-db:5432/postgres", "postgres", "mysecretpassword");
+        Sql2o sql2o = new Sql2o(getEnv("DB_URL"), getEnv("DB_USER"), getEnv("DB_PASSWORD"));
         var query = new GetAllStops(sql2o);
         var app = Javalin.create().start(80);
         app.get("/api/stops", ctx -> ctx.json(query.execute()));
@@ -19,5 +16,16 @@ public class App {
             var body = ctx.bodyAsClass(Query.class);
             ctx.status(200);
         });
+    }
+
+    private static String getEnv(String name) {
+        var value = System.getenv(name);
+
+        if (value == null) {
+            final String message = "Environment variable `%s` is required.";
+            throw new RuntimeException(String.format(message, name));
+        }
+
+        return value;
     }
 }
